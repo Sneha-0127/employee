042e53550dc8507701ff9e9627d4217f4ad46efd
Sneha-0127 <snehamk27112005@gmail.com>
2025-04-15 10:08:05 +0530

    modified

--- cie/CalculatorSimple.java
+++ cie/CalculatorSimple.java
@@ -13,5 +13,6 @@ System.out.println("Addition :"+(a+b));
 System.out.println("Subtraction :"+(a-b));
 System.out.println("Multiplication :"+(a*b));
 System.out.println("Division :"+(a/b));
+System.out.println("this is a master branch");
 }
 }
